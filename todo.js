6fe05a234f26a8715699d486379c328ce44df608
Tomáš Križák <tomaskrizak@gmail.com>
2023-07-02 22:32:10 +0200

    changed onclick to addEventListener

--- todo.js
+++ todo.js
@@ -20,17 +20,27 @@ function renderTodoList() {
     const html = `
       <div>${name}</div>
       <div>${dueDate}</div>
-      <button onclick="
-        todoList.splice(${index}, 1);
-        renderTodoList();
-        saveToStorage();
-      " class="delete-todo-button">Delete</button>
+      <button class="delete-todo-button js-delete-todo-button">Delete</button>
     `;
     todoListHTML += html;
   });
 
   document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+
+  document
+    .querySelectorAll('.js-delete-todo-button')
+    .forEach((deleteButton, index) => {
+      deleteButton.addEventListener('click', () => {
+        todoList.splice(index, 1);
+        renderTodoList();
+        saveToStorage();
+      });
+    });
 }
+
+document.querySelector('.js-add-todo-button').addEventListener('click', () => {
+  addTodo();
+});
 
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
